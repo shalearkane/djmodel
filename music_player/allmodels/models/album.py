abae09efa58d75c419785f16b108f43c123effe5
Soumik Dutta <shalearkane@gmail.com>
2022-08-18 00:35:31 +0530

    updated pre-commit
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- music_player/allmodels/models/album.py
+++ music_player/allmodels/models/album.py
@@ -4,6 +4,7 @@ from django.db.models.deletion import SET
 
 from .artist import Artist
 from .models import Genre
+from .user import User
 
 
 class Album(models.Model):
@@ -11,6 +12,15 @@ class Album(models.Model):
     genre = models.ForeignKey(Genre, related_name="album_genre", on_delete=SET(1))
     album_title = models.CharField(max_length=500)
     album_logo = models.FileField(upload_to="album", default=settings.MEDIA_ROOT + "/album_art/default.png")
+    liked_by = models.ManyToManyField(User, through="AlbumLikes")
 
     def __str__(self) -> str:
         return self.album_title + " - " + self.artist.name
+
+
+class AlbumLikes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="members")
+
+    class Meta:
+        unique_together = ("user", "album")
